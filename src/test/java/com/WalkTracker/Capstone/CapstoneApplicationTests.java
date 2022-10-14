27e8132a2e05d567e6bdde6452a2cec585140ad3
CiaranJRobinson <ciaranjayrobinson@gmail.com>
2022-10-14 18:30:21 -0400

    initial commit

---
+++ src/test/java/com/WalkTracker/Capstone/CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WalkTracker.Capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
