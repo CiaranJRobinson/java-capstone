1c3309e4f619302ebfd241bb8630f0e351626ca3
CiaranJRobinson <ciaranjayrobinson@gmail.com>
2022-10-25 11:50:15 -0400

    controller

---
+++ src/main/java/com/WalkTracker/Capstone/controllers/StepsController.java
@@ -0,0 +1,25 @@
+package com.WalkTracker.Capstone.controllers;
+
+import com.WalkTracker.Capstone.dtos.StepsDto;
+import com.WalkTracker.Capstone.services.StepsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/steps")
+public class StepsController {
+    @Autowired
+    private StepsService stepsService;
+
+    @GetMapping("/user/{userId}")
+    public List<StepsDto> getStepsByUser(@PathVariable Long userId){
+        return stepsService.getAllStepsByUserId(userId);
+    }
+
+    @PostMapping("/user/{userId}")
+    public void addSteps(@RequestBody StepsDto stepsDto,@PathVariable Long userId){
+        stepsService.addSteps(stepsDto, userId);
+    }
+}
