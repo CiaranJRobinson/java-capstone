229d30182ba57a4fefe63e2107427107744c8897
CiaranJRobinson <ciaranjayrobinson@gmail.com>
2022-10-27 17:48:35 -0400

    working on Y axis issues and towards importing steps data from DB

--- src/main/resources/static/javascript/steps.js
+++ src/main/resources/static/javascript/steps.js
@@ -1,19 +1,6 @@
 // const dataValue =;
 
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-];
+
 //data
 const data = {
     labels: ['2021-01-01', '2022-01-01', '2022-11-11'],
@@ -21,26 +8,27 @@ const data = {
     label: 'Steps through the Year',
     backgroundColor: 'rgb(97, 133, 100)',
     borderColor: 'rgb(97, 133, 100)',
-    data: [6, 3, 9],
+    data: [6000, 3600, 9000, 8220, 9003, 10000, 20324, 45670]
     }]
 };
 //config
 const config = {
-    type: 'line',
-    data: data,
+    type: 'line',  //type of chart
+    data: data.datasets.data, //pulls data array from above
     options: {
         scales:{
             x:{
-            type: 'time',
+            type: 'time', //sets x axis to days
                 time:{
                 unit:
                     'day'
                 },
-                min: '2022-01-01',
-                max:  new Date()
+                min: '2022-01-01', //shows how far back you want to go
+                max:  new Date()  //max is the current date
             }, 
             y:{
-                beginAtZero: true
+                beginAtZero: true,
+                max: 50000
             }
             }
         }
@@ -50,3 +38,11 @@ const config = {
   const myChart = new Chart(
     document.getElementById('lineChart'),
     config);
+
+    //the select months function, 'months' parameter shows the value from the select options
+function filterChart(months){
+    // console.log(luxon.DateTime.now().plus({ months:-months.value}).toISODate())
+    myChart.config.options.scales.x.min = luxon.DateTime.now().plus({ months: -months.value }).toISODate();
+    myChart.config.options.scales.x.max = luxon.DateTime.now().toISODate();
+    myChart.update();
+}
